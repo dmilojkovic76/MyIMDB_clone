d5cdeec601f5be50148b3e03d94aef70e03e4e3d
Dušan Milojković <dmilojkovic76@gmail.com>
2019-07-06 00:52:52 +0200

    started fetcing dada

--- vanilla_js/.eslintrc.js
+++ vanilla_js/.eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
     "extends": "airbnb-base",
+    "env":{
+        "browser": true,
+        "es6": true
+    },
     "rules":{
         "indent": ["warn", "tab"],
         "no-console": "off",
         "no-tabs": "off",
-        "browser": "true",
     },
 };
--- vanilla_js/app.js
+++ vanilla_js/app.js
@@ -1,14 +1,30 @@
 const OMDB_API_URL = 'http://www.omdbapi.com/?apikey=85055747&';
 
-const h1 = document.querySelector('h1');
 const searchForm = document.querySelector('form');
+const searchBox = document.querySelector('#search_box')
 
-h1.textContent = `${h1.textContent} using Vanilla JS.`;
+let loading = false;
 
 function startTheSearch(e) {
+	loading = true;
 	e.preventDefault();
-	console.log('Form Submitted!');
-	console.log(searchForm.elements);
+	fetch(`${OMDB_API_URL}t=${searchBox.value}`)
+	.then((res) => {
+		if (res.ok) {
+			return res.json();
+		}
+		throw new Error('Network response was not OK!');
+	})
+	.then((resp) => {
+		const results = document.querySelector('#results');
+		const data = JSON.stringify(resp);
+		results.textContent = data;
+		for (const r in data) {
+			console.log(r);
+		}
+	})
+	.catch(err => console.error(err));
+	loading = false;
 }
 
 searchForm.addEventListener('submit', startTheSearch);
