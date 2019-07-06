8f5b76f466c310ee0131c4baaf74274bb9ac803f
Dusan Milojkovic <dmilojkovic76@gmail.com>
2019-07-06 14:14:17 +0200

    added options to the search

--- README.md
+++ README.md
@@ -8,7 +8,18 @@ There are more than one project inside:
 
 
 For OMDB, use [this link](http://www.omdbapi.com/apikey.aspx?__EVENTTARGET=freeAcct&__EVENTARGUMENT=&__LASTFOCUS=&__VIEWSTATE=%2FwEPDwUKLTIwNDY4MTIzNQ9kFgYCAQ9kFgICBw8WAh4HVmlzaWJsZWhkAgIPFgIfAGhkAgMPFgIfAGhkGAEFHl9fQ29udHJvbHNSZXF1aXJlUG9zdEJhY2tLZXlfXxYDBQtwYXRyZW9uQWNjdAUIZnJlZUFjY3QFCGZyZWVBY2N0x0euvR%2FzVv1jLU3mGetH4R3kWtYKWACCaYcfoP1IY8g%3D&__VIEWSTATEGENERATOR=5E550F58&__EVENTVALIDATION=%2FwEdAAU5GG7XylwYou%2BzznFv7FbZmSzhXfnlWWVdWIamVouVTzfZJuQDpLVS6HZFWq5fYpioiDjxFjSdCQfbG0SWduXFd8BcWGH1ot0k0SO7CfuulN6vYN8IikxxqwtGWTciOwQ4e4xie4N992dlfbpyqd1D&at=freeAcct&Email=) to get the API KEY
+
 All data requests go to:
 http://www.omdbapi.com/?apikey=[yourkey]&
-And for the poster requests, use:
-http://img.omdbapi.com/?apikey=[yourkey]& but you need to be a patreon of OMDB.
+
+There are two (2) types of requests than be mabe to the API:
+
+- By ID or Title - where parameters _i_ or _t_ must me set to IMDB ID or Movie title to search for.
+- By Search - where parameter _s_ must me set to a term to search for.
+
+The return value asked for can be filtered by:
+
+- type : movie, series or episode
+- y : set this to year of release
+- plot : short or full - in case if _i_ or _t_ parametars were used
+- page : which page to return 1..100, defaults to 1 - in case if _s_ parameter was used
--- vanilla_js/app.js
+++ vanilla_js/app.js
@@ -1,30 +1,41 @@
 const OMDB_API_URL = 'http://www.omdbapi.com/?apikey=85055747&';
 
 const searchForm = document.querySelector('form');
-const searchBox = document.querySelector('#search_box')
-
-let loading = false;
+const searchBox = document.querySelector('#search_box');
+const searchType = document.querySelector('#search-type');
 
 function startTheSearch(e) {
-	loading = true;
 	e.preventDefault();
-	fetch(`${OMDB_API_URL}t=${searchBox.value}`)
-	.then((res) => {
-		if (res.ok) {
-			return res.json();
-		}
-		throw new Error('Network response was not OK!');
-	})
-	.then((resp) => {
-		const results = document.querySelector('#results');
-		const data = JSON.stringify(resp);
-		results.textContent = data;
-		for (const r in data) {
-			console.log(r);
-		}
-	})
-	.catch(err => console.error(err));
-	loading = false;
+	let param = 's';
+	let type = '';
+	if (searchType.value === 'titles') {
+		param = 't';
+		type = 'm';
+	} else if (searchType.value === 'episode') {
+		param = 't';
+		type = 'episode';
+	} else if (searchType.value === 'series') {
+		param = 't';
+		type = 'series';
+	} else if (searchType.value === 'imdbs') {
+		param = 'i';
+		type = '';
+	}
+	const FULL_OMDB_URL = `${OMDB_API_URL}${param}='${searchBox.value}'&type=${type}`;
+	console.log(FULL_OMDB_URL);
+	fetch(FULL_OMDB_URL)
+		.then((res) => {
+			if (res.ok) {
+				return res.json();
+			}
+			throw new Error('Network response was not OK!');
+		})
+		.then((resp) => {
+			const results = document.querySelector('#results');
+			const data = JSON.stringify(resp);
+			results.textContent = data;
+		})
+		.catch(err => console.error(err));
 }
 
 searchForm.addEventListener('submit', startTheSearch);
