46b34515d3ad909f05815a374cab7fbc7cdfe8fb
Dusan Milojkovic <dmilojkovic76@gmail.com>
2019-07-10 22:09:55 +0200

    added some helper functions

--- vanilla_js/app.js
+++ vanilla_js/app.js
@@ -3,7 +3,8 @@ const OMDB_API_URL = 'http://www.omdbapi.com/?apikey=85055747&';
 const searchForm = document.querySelector('form');
 const searchBox = document.querySelector('#search-box');
 const searchType = document.querySelector('#search-type');
-const results = document.querySelector('#results');
+const resultsDiv = document.querySelector('#results');
+const realtimeSearchDiv = document.querySelector('#realtime-search');
 
 function checkSearchType(value) {
 	const obj = {};
@@ -26,6 +27,11 @@ function checkSearchType(value) {
 	return obj;
 }
 
+function buildURL() {
+	const params = checkSearchType(searchType.value);
+	return `${OMDB_API_URL}${params.param}='${searchBox.value}'&type=${params.type}`;
+}
+
 function createElement(elem, content, src) {
 	const element = document.createElement(elem);
 	if (elem === 'img') {
@@ -35,6 +41,7 @@ function createElement(elem, content, src) {
 		element.setAttribute('height', '300px');
 	} else if (elem === 'div') {
 		element.classList.add(content);
+		// eslint-disable-next-line no-unused-expressions
 		(src) ? element.id = src : src;
 	} else {
 		element.innerText = content;
@@ -42,10 +49,9 @@ function createElement(elem, content, src) {
 	return element;
 }
 
-function startTheSearch(e) {
-	e.preventDefault();
-	const params = checkSearchType(searchType.value);
-	const FULL_OMDB_URL = `${OMDB_API_URL}${params.param}='${searchBox.value}'&type=${params.type}`;
+function realtimeSearch(e) {
+	console.log('realtimeSearch Started');
+	const FULL_OMDB_URL = buildURL();
 
 	fetch(FULL_OMDB_URL)
 		.then((res) => {
@@ -56,15 +62,38 @@ function startTheSearch(e) {
 		})
 		.then((resp) => {
 			if (resp.Response) {
-				results.innerText = '';
-				results.style.visibility = 'visible';
-				results.appendChild(createElement('h2', `Number of results: ${resp.totalResults}`));
+				realtimeSearchDiv.classList.toggle('hidden');
+				realtimeSearchDiv.innerText = '';
+				realtimeSearchDiv.appendChild(createElement('h2', `Number of results: ${resp.totalResults}`));
+			} else {
+				realtimeSearchDiv.classList.toggle('hidden');
+			}
+		})
+		.catch(err => console.error(err));
+}
 
-				results.appendChild(createElement('div', 'movie-results'));
+function startTheSearch(e) {
+	e.preventDefault();
+	const FULL_OMDB_URL = buildURL();
+
+	fetch(FULL_OMDB_URL)
+		.then((res) => {
+			if (res.ok) {
+				return res.json();
+			}
+			throw new Error('Network response was not OK!');
+		})
+		.then((resp) => {
+			if (resp.Response) {
+				resultsDiv.innerText = '';
+				resultsDiv.style.visibility = 'visible';
+				resultsDiv.appendChild(createElement('h2', `Number of results: ${resp.totalResults}`));
+
+				resultsDiv.appendChild(createElement('div', 'movie-results'));
 				const movieResults = document.querySelector('.movie-results');
 				let i = 0;
 				resp.Search.forEach((movie) => {
-					movieResults.appendChild(createElement('div', `movie-result`, `movie-result-${i}`));
+					movieResults.appendChild(createElement('div', 'movie-result', `movie-result-${i}`));
 					const movieResult = document.querySelector(`#movie-result-${i}`);
 					movieResult.appendChild(createElement('img', movie.Title, movie.Poster));
 					movieResult.appendChild(createElement('p', movie.Title));
@@ -75,4 +104,5 @@ function startTheSearch(e) {
 		.catch(err => console.error(err));
 }
 
+searchBox.addEventListener('keydown', realtimeSearch);
 searchForm.addEventListener('submit', startTheSearch);
